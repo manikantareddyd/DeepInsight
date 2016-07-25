daeb3ab6ee1c93d583ed3f500f311a2c8283bfe6
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-07-25 14:19:33 +0530

    Added Scholar.py. It f***g works

--- DeepInsightWeb/App/AppController.js
+++ DeepInsightWeb/App/AppController.js
@@ -5,7 +5,7 @@
         "url": "https://api.textrazor.com",
         "method": "POST",
         "headers": {
-            "x-textrazor-key": "poo",
+            "x-textrazor-key": "b043156ea6a956b7b7cee9c9fdbead629578a609caace754ad13f958",
             "accept-encoding": "application/gzip",
             "content-type": "application/x-www-form-urlencoded"
         },
